5030aca768ddd90fcf301bc145cf094d62c3b69e
GeorgeU2030 <georgeu2021@gmail.com>
2024-06-03 14:58:12 -0500

    style: register_page_picture_was_changed

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -9,7 +9,7 @@ export default function Register() {
     <div className='min-h-screen flex flex-col bg-gradient-to-tr from-one to-red-800'>
         <div className='flex flex-col md:flex-row flex-grow'>
             <section className='flex flex-grow md:w-1/2 w-full md:items-center mt-16 md:mt-0'>
-                <Image src="https://www.hoyesarte.com/wp-content/uploads/2021/12/sala-de-cine.jpg" className='md:ml-2 border-four border-4' />
+                <Image src="https://preview.redd.it/2jhtmqhg4mo81.png?width=1920&format=png&auto=webp&s=0d41709c3c478d2bcadfd8f2450271f175c0676f" className='md:ml-2 border-four border-4' />
             </section>
             <section className='flex flex-grow md:w-1/2 w-full md:items-center justify-center '>
                 <div className='bg-five w-5/6 md:w-3/5 rounded-lg mt-6 mb-6'>
